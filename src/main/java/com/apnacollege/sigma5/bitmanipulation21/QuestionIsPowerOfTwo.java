5659893ace2eabfc6a1dd15f2e3bc99a351cfb71
Afreen Kausar <afreenkausar2501@gmail.com>
2026-09-18 02:09:38 +0530

    solve count set bits

---
+++ src/main/java/com/apnacollege/sigma5/bitmanipulation21/QuestionIsPowerOfTwo.java
@@ -0,0 +1,12 @@
+package com.apnacollege.sigma5.bitmanipulation21;
+
+public class QuestionIsPowerOfTwo {
+    
+    public static boolean isPowerOfTwo(int n){
+        return (n&(n-1)) == 0;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(8));
+        System.out.println(isPowerOfTwo(15));
+    }
+}
