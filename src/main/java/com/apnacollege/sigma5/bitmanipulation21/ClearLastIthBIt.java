6770a0366e1870276e64c7305ba62842927b468d
Afreen Kausar <afreenkausar2501@gmail.com>
2026-09-18 01:37:20 +0530

    add clear range of bits

---
+++ src/main/java/com/apnacollege/sigma5/bitmanipulation21/ClearLastIthBIt.java
@@ -0,0 +1,12 @@
+package com.apnacollege.sigma5.bitmanipulation21;
+
+public class ClearLastIthBIt {
+    public static int clearIthBit(int n, int i){
+        int bitMask = (~0)<<i;
+        return n & bitMask;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(clearIthBit(15, 2));
+    }
+}
